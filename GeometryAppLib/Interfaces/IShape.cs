11484bb2bd9d860fa6439717f3a854b9b3986e2c
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-04-17 10:59:17 +0000

    Interface defined

---
+++ GeometryAppLib/Interfaces/IShape.cs
@@ -0,0 +1,9 @@
+namespace GeometryApp.GeometryAppLib.interfaces;
+/// <summary>
+/// Interface that can be implemented by classes of different shapes.
+/// </summary>
+
+public interface IShape{
+    double CalculateArear();
+    double CalculatePerimeter();
+}
