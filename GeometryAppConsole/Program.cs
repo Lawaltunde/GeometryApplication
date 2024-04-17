d978b94c3e566d1f8349990f2acda59926a128b9
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-04-17 16:45:23 +0000

    Dependency Injection package installed

--- GeometryAppConsole/Program.cs
+++ GeometryAppConsole/Program.cs
@@ -1,3 +1,4 @@
 ﻿
 
 var featureManagement = new Dictionary<string, string> {{ "FeatureManagement:Square", "true"}, { "FeatureManagement:Rectangle", "false"}, { "FeatureManagement:Triangle", "true"}};
+//IConfigurationRoot configuration = new ConfigurationBuilder().AddInMemoryCollection(featureManagement).Build();
